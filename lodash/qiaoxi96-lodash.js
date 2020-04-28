463f6c4a0d9098dddf98f5fd463dd6e43e8c5469
gold <you@example.com>
2020-04-28 15:11:11 +0800

    kk

---
+++ lodash/qiaoxi96-lodash.js
@@ -0,0 +1,16 @@
+var qiaoxi96 = {
+  isNull: function (val) {
+    if (val == null) {
+      return true
+    } else {
+      return false
+    }
+  },
+  isNaN: function (val) {
+    if (val !== val) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
