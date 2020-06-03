15cc8791b57936be7075b4c36d5c045e001c1c5b
gold <you@example.com>
2020-06-03 09:18:38 +0800

    loadsh

--- lodash/qiaoxi96-lodash.js
+++ lodash/qiaoxi96-lodash.js
@@ -1,6 +1,7 @@
 //lodash函数实现
 
 var qiaoxi96 = {
+
   isNull: function (val) {
     if (val === null) {
       return true
@@ -14,5 +15,25 @@ var qiaoxi96 = {
     } else {
       return false
     }
-  }
+  },
+  chunk: function (arry, size = 1) {
+    var arry2 = []
+    for (var i = 0; i < arry.length; i += size) {
+      arry2.push(array.slice(i, i + size))
+    }
+    return arry2
+  },
+  compack: function (arry) {
+    var arr = []
+    for (var i = 0; i < arry.length; i++) {
+      if (arry[i]) {
+        arry2.push(arry[i])
+      }
+    }
+  },
+
+
+
 }
+
+
