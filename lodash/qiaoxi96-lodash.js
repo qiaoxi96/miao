2b1efb17525a2030ce2730b2980d20779f36d42f
gold <you@example.com>
2020-06-03 09:22:18 +0800

    new loadsh

--- lodash/qiaoxi96-lodash.js
+++ lodash/qiaoxi96-lodash.js
@@ -1,6 +1,22 @@
 //lodash函数实现
 
 var qiaoxi96 = {
+  chunk: function (array, size) {
+    var arry2 = []
+    for (var i = 0; i < array.length; i += size) {
+      arry2.push(array.slice(i, i + size))
+    }
+    return arry2
+  },
+  compack: function (array) {
+    var arr = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        arr.push(array[i])
+      }
+    }
+  },
+
 
   isNull: function (val) {
     if (val === null) {
@@ -16,23 +32,6 @@ var qiaoxi96 = {
       return false
     }
   },
-  chunk: function (arry, size = 1) {
-    var arry2 = []
-    for (var i = 0; i < arry.length; i += size) {
-      arry2.push(array.slice(i, i + size))
-    }
-    return arry2
-  },
-  compack: function (arry) {
-    var arr = []
-    for (var i = 0; i < arry.length; i++) {
-      if (arry[i]) {
-        arry2.push(arry[i])
-      }
-    }
-  },
-
-
 
 }
 
